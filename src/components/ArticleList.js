076585019c4c7b50358a00a9d754c7a106234d2c
Karter <kdlmuzixnerd14@gmail.com>
2022-07-20 10:53:03 -0700

    commit tree 83b12b9c84b79bf017548041a4963c0b139899f2

---
+++ src/components/ArticleList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from "./App";
+import Article from "./Article";
+
+function ArticleList({posts})
+{
+    return <main>
+        {posts.map(post =>{
+          return <Article key={post.id} title={post.title} date={post.date} preview={post.preview} minutes={post.minutes} />})}
+    </main>
+}
+
+export default ArticleList;
